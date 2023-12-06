a20d6f04f41ec09c731c7059a1ef230200f6e6d3
YangYunSeok <89758411+YangYunSeok@users.noreply.github.com>
2023-12-06 12:54:28 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+
+	//freopen("input.txt", "r", stdin);
+
+	int dan;
+
+	scanf("%d", &dan);
+
+	for (int j = 1; j <= dan; j++) {
+		printf("%d", j);
+		printf("\n");
+	}
+
+}
