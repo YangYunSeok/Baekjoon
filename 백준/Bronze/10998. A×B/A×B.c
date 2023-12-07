2609170610af9324dae1984d41ee426f605d085c
YangYunSeok <89758411+YangYunSeok@users.noreply.github.com>
2023-12-07 16:41:41 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#pragma warning(disable:4996)
+
+int main() {
+
+	//freopen("input.txt", "r", stdin);
+
+	int T;
+	int num1 = 0, num2 = 0;
+	int flag = 1;
+
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d\n", num1 * num2);
+}
